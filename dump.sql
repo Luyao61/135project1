aa49e241ca2105914630b7a9828f2c5bf1268491
Luyao Zhou <l4zhou@ucsd.edu>
2015-04-26 13:37:09 -0700

    add foreign key to categories; delete signup

--- dump.sql
+++ dump.sql
@@ -9,20 +9,6 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
@@ -30,83 +16,24 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: account; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE account (
-    name character varying(20) NOT NULL,
-    role character(20) NOT NULL,
-    age integer NOT NULL,
-    state character(20) NOT NULL
-);
-
-
-ALTER TABLE account OWNER TO postgres;
-
---
 -- Name: categories; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
 CREATE TABLE categories (
-    name character(20) NOT NULL,
-    description character(200),
-    owner character varying(20)
+    name text NOT NULL,
+    description text,
+    owner text
 );
 
 
 ALTER TABLE categories OWNER TO postgres;
 
 --
--- Name: signup; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-
-CREATE TABLE signup (
-    name character(20),
-    role character(20),
-    age integer,
-    state character(20) NOT NULL
-);
-
-
-ALTER TABLE signup OWNER TO postgres;
-
---
--- Data for Name: account; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO account VALUES ('z', 'O                   ', 20, 'C                   ');
-INSERT INTO account VALUES ('zhou', 'Owner               ', 12, 'AL                  ');
-INSERT INTO account VALUES ('q', 'Owner               ', 12, 'AL                  ');
-INSERT INTO account VALUES ('zhang', 'Owner               ', 1, 'CA                  ');
-
-
---
 -- Data for Name: categories; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO categories VALUES ('b                   ', 'sadas                                                                                                                                                                                                   ', 'zhang');
-INSERT INTO categories VALUES ('nnn                 ', 'kkkk                                                                                                                                                                                                    ', 'zhou');
-INSERT INTO categories VALUES ('bbbb                ', 'hfddjk                                                                                                                                                                                                  ', 'zhou');
-INSERT INTO categories VALUES ('a                   ', 'a                                                                                                                                                                                                       ', 'zhang');
-INSERT INTO categories VALUES ('c                   ', 'c                                                                                                                                                                                                       ', 'zhang');
-INSERT INTO categories VALUES ('d                   ', 'd                                                                                                                                                                                                       ', 'zhang');
-INSERT INTO categories VALUES ('e                   ', 'e                                                                                                                                                                                                       ', 'zhang');
-INSERT INTO categories VALUES ('f                   ', 'f                                                                                                                                                                                                       ', 'zhang');
-INSERT INTO categories VALUES ('g                   ', 'g                                                                                                                                                                                                       ', 'zhang');
-
-
---
--- Data for Name: signup; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Name: account_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
---
-
-ALTER TABLE ONLY account
-    ADD CONSTRAINT account_pkey PRIMARY KEY (name);
+COPY categories (name, description, owner) FROM stdin;
+\.
 
 
 --
@@ -118,21 +45,11 @@ ALTER TABLE ONLY categories
 
 
 --
--- Name: signup_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
+-- Name: categories_owner_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY signup
-    ADD CONSTRAINT signup_pkey PRIMARY KEY (state);
-
-
---
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT categories_owner_fkey FOREIGN KEY (owner) REFERENCES account(name);
 
 
 --
